a2a4cc9920a448c8703bcfe87b31e6430f6b90ea
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-05-22 22:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 st.set_page_config(page_title="AI Resume Auth App", layout="centered")
 
-api = st.input_text("ENTER YOUR OPENAI API KEY")
+#api = st.input_text("ENTER YOUR OPENAI API KEY")
 
 api = st.input_text(placeholder="ENTER YOUR OPENAI API",max_chars=48)
 
