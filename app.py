eb6b42cc28d003fe060a40d6ffe9c440abeb2035
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-05-23 06:00:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import openai
 
 st.set_page_config(page_title="AI Resume Auth App", layout="centered")
 
-api = st.text_input("OPENAI API KEY",placeholder="ENTER YOUR OPENAI API", max_chars=48, type="password")
+api = st.text_input("OpenAI API Key", placeholder="ENTER YOUR OPENAI API", max_chars=48)
 
 openai.api_key = api
 
